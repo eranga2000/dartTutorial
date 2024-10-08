236df833de536ec8ae0cf3e30d19015a4f751e79
eranga2000 <dmemadhushan@std.appsc.sab.ac.lk>
2024-10-08 17:16:12 +0530

    get started

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,9 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+  //Dart requires a main function, it is entry point to your dart application.
+  // print('Hello world: ${dart_application_1.calculate()}!');//
+
+  dart_application_1.printSomething();
 }
