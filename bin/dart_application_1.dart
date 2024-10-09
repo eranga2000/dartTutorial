9df82bde1cee1348afcdd700c3fd0bc6b4516a92
eranga2000 <dmemadhushan@std.appsc.sab.ac.lk>
2024-10-09 16:04:46 +0530

    using if else

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,11 +1,10 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
+import 'package:dart_application_1/ifELse.dart';
 
 void main(List<String> arguments) {
   //Dart requires a main function, it is entry point to your dart application.
   // print('Hello world: ${dart_application_1.calculate()}!');//
 
-  dart_application_1.printSomething();
-  
-  
+  usingIfElse();
 }
---
+++ lib/ifELse.dart
@@ -0,0 +1,19 @@
+void usingIfElse() {
+  int marks = 67;
+  bool isOkay = marks < 100 && marks > 0;
+  if (isOkay) {
+    if (marks >= 75) {
+      print('A');
+    } else if (marks >= 65) {
+      print('B');
+    } else if (marks >= 50) {
+      print('C');
+    } else if (marks >= 35) {
+      print('S');
+    } else {
+      print('W');
+    }
+  } else {
+    print('enter the right marks');
+  }
+}
